34f85e82c71f61b5b3608be8f4ebd6ac16a6a6ae
Theresa Karamor <teriekarie@gmail.com>
2022-07-16 16:00:20 +0100

    First push

---
+++ Assignment_pratice.py
@@ -0,0 +1,7 @@
+# python program to see the return value
+def test(val):
+    if 0 == val:
+        return 7
+    a= test(6)
+    print 'a'
+    
---
+++ python_datatypes.py
@@ -0,0 +1,58 @@
+# python program for
+# creating string
+
+# creating a string
+# with single quotes
+from pickletools import string1
+import string
+
+
+string1 = 'welcome to Nigeria'
+print("stingd with the use of single quotes: ")
+print(string1)
+
+# creating a string
+# with triple quotes
+string1 = ' ' 'We are from Nigeria a world of "Love" ' ' '
+print(string1)
+print (type(string1))
+
+# creating a string with double quotes
+string1 = "This is Nigeria and I am not very proud of it"
+print(string1)
+print(type(string1))
+
+# creating a sting with tripple quotes
+# quotes allows multiple lines
+string1 = '''Friends 
+for 
+life '''
+print("\ncreating a multiline string: ")
+print(string1)
+
+
+# python program to acess characters of string
+
+string1 = "Music is Life"
+print("initial string: ")
+print(string1)
+
+# printing first character
+print("\nfirst character of string is: ")
+print(string1[0])
+
+# printing last character
+print("\nlast character of string is: ")
+print(string1[-1])
+
+# creating program to demonstrate creation of list
+
+# creating a list
+list = []
+print("initial blank list: ")
+print(list)
+
+# creating a lsit with the use of a string
+list = ['Friends for life']
+print(list)
+
